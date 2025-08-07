0189862ee63708093e64be1859a15464aec1ab82
Lucas Yago <lucasyago63@gmail.com>
2025-08-06 23:28:49 -0300

    Implementa correção na funcionalidade de persistir livro

--- src/main/java/br/com/alura/literalura/service/BookService.java
+++ src/main/java/br/com/alura/literalura/service/BookService.java
@@ -1,12 +1,15 @@
 package br.com.alura.literalura.service;
 
+import br.com.alura.literalura.model.Author;
 import br.com.alura.literalura.model.Book;
 import br.com.alura.literalura.repository.AuthorRepository;
 import br.com.alura.literalura.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -17,7 +20,12 @@ public class BookService {
     @Autowired
     private AuthorRepository authorRepository;
 
+    @Transactional
     public void saveBook(Book book){
+        Optional<Author> authorExist = authorRepository.findByNameContainingIgnoreCase(book.getAuthor().getName());
+        if (authorExist.isPresent()) {
+            book.setAuthor(authorExist.get());
+        }
         repository.save(book);
     }
 
